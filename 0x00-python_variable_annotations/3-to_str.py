b948c30676073e7b9043378508e57232d368a5dc
Iheb Timoumi <55882164+timoumi12@users.noreply.github.com>
2024-01-26 00:05:35 +0100

    task3

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Contains a function that returns a string representation of a float"""
+"""Contains a function that returns string representation of a float"""
 
 
 def to_str(n: float) -> str:
